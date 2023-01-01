e6eea7027cf4e973e82ec55e9fb8d9ab2f5e1172
Timothy Cates <Timothyccates1@gmail.com>
2023-01-01 10:34:29 -0700

    Rather than requiring user to run script, simply source the next script and continue

--- setup.sh
+++ setup.sh
@@ -9,16 +9,14 @@ REPO="https://raw.githubusercontent.com/timothycates/ArchInstall/main/"
 clear
 if confirm "Have you formatted and mounted your drives?"
 then
- curl -SL $REPO/scripts/prechroot.sh -o prechroot.sh
- chmod +x prechroot.sh
+ curl -sL $REPO/scripts/prechroot.sh -o prechroot.sh
+ source prechroot.sh
 else
   clear
   echo "This script requires drvies to be pre-formated and mounted to /mnt to continue"
   echo "If you need help follow the wiki"
   exit
 fi
-
-echo $REPO
 
 
 
