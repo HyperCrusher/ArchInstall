465c199c191fbfa15c43e2128e47f94c6faffec4
Timothy Cates <Timothyccates1@gmail.com>
2023-01-01 10:25:30 -0700

    Added a confirmation function script

---
+++ scripts/utils/confirm.sh
@@ -0,0 +1,19 @@
+confirm() {
+  local _prompt _default _response
+ 
+  if [ "$1" ]; then _prompt="$1"; else _prompt="Are you sure"; fi
+  _prompt="$_prompt [y/n] ?"
+  while true; do
+    read -r -p "$_prompt " _response
+    case "$_response" in
+      [Yy][Ee][Ss]|[Yy])
+        return 0
+        ;;
+      [Nn][Oo]|[Nn]) 
+        return 1
+        ;;
+      *) 
+        ;;
+    esac
+  done
+}
