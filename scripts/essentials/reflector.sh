3b7ea3ee24aca67050fde751738fd79a4f1c5a90
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 10:30:20 -0700

    moved reflector to its own script

---
+++ scripts/essentials/reflector.sh
@@ -0,0 +1,7 @@
+# Update our Mirrors
+pacman -S --noconfirm reflector
+sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+pacman -Syy --noconfirm
+
+#start reflector service
+sudo systemctl enable reflector.timer
