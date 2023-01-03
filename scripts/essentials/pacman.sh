2673f6999745ddbf2209c02843c4880444e23f3e
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 12:19:30 -0700

    Added downgrade and rebuild-detector with hook to pacman

--- scripts/essentials/pacman.sh
+++ scripts/essentials/pacman.sh
@@ -9,3 +9,9 @@ fi
 
 # Color
 sed -i "/#Color/s/^#//" /etc/pacman.conf
+
+# Tools to aid in using pacman / updating system
+pacman -S --noconfirm downgrade rebuild-detector
+
+# Add rebuild hook
+cp ./configs/pacman/rebuild.hook /etc/pacman.d/hooks/rebuild.hook
