708c3c7040156da5d64f0d5d01b6a47ee8093a9f
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 13:58:01 -0700

    Desktop.sh only matter if the install is for desktop use

---
+++ scripts/extras/desktop.sh
@@ -0,0 +1,8 @@
+# These are nice to have's if the install is for desktop use
+pacman -S --noconfirm xdg-user-dirs xdg-utils
+
+# Torrent Client
+pacman -S --noconfirm qbittorrent
+
+# GUI sound control
+pacman -S --noconfirm pavucontrol
