0a83324691dc76bbd38ecd8766e3bff5842ab59a
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 12:19:57 -0700

    Added termina.sh for managing things that make using the terminal better

---
+++ scripts/essentials/terminal.sh
@@ -0,0 +1,11 @@
+# Things that make the terminal better reside here
+
+# Nice to haves
+pacman -S --noconfirm curl man-db ripgrep git unzip
+
+# Format/query json via cli
+pacman -S --noconfirm jq jql
+
+# Rust alternatives to common utils
+pacman -S --noconfirm bat exa fd
+
