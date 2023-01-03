c4e15578b98d636ff7adefd60033e4c7b56f7236
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 11:50:41 -0700

    Remove sudo (again) since I can't read

--- scripts/essentials/reflector.sh
+++ scripts/essentials/reflector.sh
@@ -1,6 +1,6 @@
 # Update our Mirrors
 pacman -S --noconfirm reflector
-sudo reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syy --noconfirm
 
 #start reflector service
