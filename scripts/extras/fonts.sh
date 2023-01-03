81f82826c32932411670755adb04ba3c4ce7bd30
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 10:47:46 -0700

    Setup fonts.sh

---
+++ scripts/extras/fonts.sh
@@ -0,0 +1,19 @@
+
+# Install 'base' fonts
+pacman -S --noconfirm adobe-source-code-pro-fonts cantarell-fonts ttf-ms-fonts ttf-opensans
+
+# Korean Font(s)
+clear
+if confirm "Install Korean Fonts?"
+then
+  pacman -S --noconfirm ttf-baekmuk
+fi
+
+# Nerd fonts
+git clone --depth 1 https://www.github.com/ryanoasis/nerd-fonts
+cd nerd-fonts
+./install.sh
+
+# Cleanup nerd-fonts
+cd /
+rm -r nerd-fonts
