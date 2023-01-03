fc8827ede4dc881f830175e10b28b73ab90e38bb
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 12:26:34 -0700

    added dnsmasq

--- scripts/essentials/network.sh
+++ scripts/essentials/network.sh
@@ -2,7 +2,7 @@
 pacman -S --noconfirm network-manager
 
 # Network nice-to-haves
-pacman -S --noconfirm avahi iptables-nft openssh
+pacman -S --noconfirm avahi iptables-nft openssh dnsmasq
 
 # Network attached storage
 pacman -S --noconfirm nfs-utils
