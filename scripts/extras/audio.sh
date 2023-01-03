9f6d5d9fcb0bf01b25052e56fecb7e2a3df23925
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 11:29:48 -0700

    Moved audio packages to audio.sh also migrated from pipewire-media-session to wireplumber

---
+++ scripts/extras/audio.sh
@@ -0,0 +1,6 @@
+# Install alsa related packages
+pacman -S --noconfirm alsa-utils asla-firmware alsa-plugins
+
+# Install pipewire
+pacman -S --noconfirm  pipewire pipewire-alsa pipewire-jack pipewire-pulse wireplumber
+
