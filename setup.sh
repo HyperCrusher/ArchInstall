a431089acd557a366c8980611682d7aea9ec6077
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 00:28:59 -0700

    Moving away from a curl centric setup and to one that relies on git

--- setup.sh
+++ setup.sh
@@ -4,13 +4,10 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 source "$DIR/scripts/utils/confirm.sh"
 
-REPO="https://raw.githubusercontent.com/timothycates/ArchInstall/main/"
 
-clear
 if confirm "Have you formatted and mounted your drives?"
 then
- curl -sL $REPO/scripts/prechroot.sh -o prechroot.sh
- source prechroot.sh
+ source "$DIR/scripts/prechroot.sh"
 else
   clear
   echo "This script requires drvies to be pre-formated and mounted to /mnt to continue"
