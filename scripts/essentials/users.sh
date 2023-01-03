7dd08cf0520e82f623e616a7e714f08cdabd0cdb
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 11:00:33 -0700

    Setup the wheel group to gain sudo access

--- scripts/essentials/users.sh
+++ scripts/essentials/users.sh
@@ -13,3 +13,7 @@ useradd --badname -m -g users -G wheel,ftp,http $USERNAME
 clear
 echo "Password:"
 passwd $USERNAME
+
+# give wheel group memebers access to sudo 
+sed --in-place 's/^#\s*\(%wheel\s\+ALL=(ALL:ALL)\s\+ALL\)/\1/' /etc/sudoers
+
