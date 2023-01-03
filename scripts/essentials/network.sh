896978722c2930e72721bdd9b10c0a8a498a926a
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 11:48:47 -0700

    Added nfs-utils

--- scripts/essentials/network.sh
+++ scripts/essentials/network.sh
@@ -1,7 +1,11 @@
 # Install network manager
 pacman -S --noconfirm network-manager
 
+# Network nice-to-haves
 pacman -S --noconfirm avahi iptables-nft openssh
+
+# Network attached storage
+pacman -S --noconfirm nfs-utils
 
 # Enable network-manager service
 systemctl enable NetworkManager
