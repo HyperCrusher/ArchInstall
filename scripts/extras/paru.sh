4fc0c29a64a73bfb5a381dbf176edc695a2a830c
Timothy Cates <Timothyccates1@gmail.com>
2023-01-03 17:04:37 -0700

    Paru setup

--- scripts/extras/paru.sh
+++ scripts/extras/paru.sh
@@ -1,1 +1,13 @@
+# Setup paru
+cd /
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# cp our config to the correct place
+cp "$DIR/configs/paru/paru.conf" /etc/paru.conf
+
+# clean up
+cd /
+rm -r paru
 
