8e037cf9cde9e93ab354551caf9c4673039d8959
TimothyCates <timothyccates1@gmail.com>
2023-01-05 15:46:00 +0000

    Remove korean fonts for now

--- scripts/extras/fonts.sh
+++ scripts/extras/fonts.sh
@@ -6,11 +6,7 @@ sudo pacman -S --noconfirm adobe-source-code-pro-fonts cantarell-fonts ttf-ms-fo
 sudo pacman -S --noconfirm ttf-liberation
 
 # Korean Font(s)
-clear
-if confirm "Install Korean Fonts?"
-then
-  sudo pacman -S --noconfirm ttf-baekmuk
-fi
+# sudo pacman -S --noconfirm ttf-baekmuk
 
 # Nerd fonts
 git clone --depth 1 https://www.github.com/ryanoasis/nerd-fonts
