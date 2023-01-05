8c3c8cadb706c4109a3e298b53d6314128b8a157
TimothyCates <timothyccates1@gmail.com>
2023-01-05 08:26:44 +0000

    Merged reflector into pacman.sh Added a reflector.conf with our preffered defaults

--- scripts/essentials/pacman.sh
+++ scripts/essentials/pacman.sh
@@ -1,17 +1,38 @@
 # Setup Pacman
 
+# Tools to aid in using pacman / updating system
+pacman -S --noconfirm rebuild-detector reflector
+
+# Make the reflector folder if it doesnt exist
+mkdir -p /etc/xdg/reflector
+
+# Copy our conf over
+cp "$DIR/configs/pacman/reflector.conf" /etc/xdg/reflector/reflector.conf
+
+# Add our country filter
+echo "--country $REFLECTOR_COUNTRY" >> /etc/xdg/reflector/reflector.conf
+
+# Update mirrorlist right now
+reflector --country "$REFLECTOR_COUNTRY" --latest 100 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+# Enable Color
+sed -i "/#Color/s/^#//" /etc/pacman.conf
+
 # multilib
 if confirm "Do you want to use multilib libraries?"
 then
   sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-  pacman -Syyu --noconfirm
+  MULTILIB=1
 fi
 
-# Color
-sed -i "/#Color/s/^#//" /etc/pacman.conf
+# Useful since we changed mirrorlist and may have enabled multilib
+pacman -Syyu --noconfirm
 
-# Tools to aid in using pacman / updating system
-pacman -S --noconfirm downgrade rebuild-detector
+# Make a hooks folder
+mkdir -p /etc/pacman.d/hooks
 
 # Add rebuild hook
 cp "$DIR/configs/pacman/rebuild.hook" /etc/pacman.d/hooks/rebuild.hook
+
+#start reflector service
+systemctl enable reflector.timer
--- scripts/essentials/reflector.sh
+++
@@ -1,7 +0,0 @@
-# Update our Mirrors
-pacman -S --noconfirm reflector
-reflector --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-pacman -Syy --noconfirm
-
-#start reflector service
-systemctl enable reflector.timer
