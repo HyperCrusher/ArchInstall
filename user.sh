7a8fc990b293ddb05c1e73b666be953aaf5d2c6e
Timothy Cates <timothyccates1@gmail.com>
2023-03-05 00:03:07 -0700

    Add flameshot

--- user.sh
+++ user.sh
@@ -33,7 +33,7 @@ yay -S --noconfirm ttf-ms-fonts adobe-source-code-pro-fonts ttf-jetbrains-mono n
 # Some nice packages
 yay -S --noconfirm shellcheck fend ouch monolith 
 yay -S --noconfirm abook neomutt isync msmtp pass pam-gnupg notmuch urlview
-yay -S --noconfirm github-cli stow nitch lazygit jql
+yay -S --noconfirm github-cli stow nitch lazygit jql flameshot
 yay -S --noconfirm picom-git nitrogen
 yay -S --noconfirm ly mkinitcpio-colors-git
 yay -S --noconfirm steam-native-runtime
