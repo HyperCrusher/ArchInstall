7c3700e01636f21d3516562c91d4f26cc93e61b6
Hyper <hypercrusher@proton.me>
2024-07-16 19:33:21 -0700

    Fix path to pacman.conf

--- src/setup/initial.sh
+++ src/setup/initial.sh
@@ -6,6 +6,6 @@ pacman-key --init
 pacman-key --populate archlinux
 
 rm /mnt/etc/pacman.conf
-cp ../../configs/pacman.conf /mnt/etc/pacman.conf
+cp configs/pacman.conf /mnt/etc/pacman.conf
 
 genfstab -U /mnt >> /mnt/etc/fstab
